27cd4efd6a41b1d9272008bb8d6de808ca9ebf12
joe4334w <efraincolquee@gmail.com>
2022-05-31 19:11:31 -0400

    t

---
+++ basicos/b1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a, b, T ;
+
+  cout << "Escriba un numero ";
+  cin >> a ;
+  cout << "Escriba un numero ";
+  cin >> b ;
+
+  T = 2 + a + b ;
+
+
+  cout << "Mostrar el resultado : " << T ;
+}
